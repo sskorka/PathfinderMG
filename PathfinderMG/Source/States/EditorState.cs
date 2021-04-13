09e6f2d30dc5decc296945ca002cfad3352be1cc
Szymon Skórka <szymonskorka@outlook.com>
2021-04-13 17:27:12 +0200

    Move default scenario generation to a method
    * This now generates Start and Target nodes, without which Grid cannot be generated

--- PathfinderMG/Source/States/EditorState.cs
+++ PathfinderMG/Source/States/EditorState.cs
@@ -12,31 +12,6 @@ namespace PathfinderMG.Core.Source.States
     {
         private const int DEFAULT_ROWS_COLS = 4;
 
-        private ScenarioWrapper defaultScenario
-        {
-            get
-            {
-                List<string> scenarioData = new List<string>();
-                StringBuilder sb = new StringBuilder();
-
-                for (int i = 0; i < DEFAULT_ROWS_COLS; i++)
-                {
-                    for (int j = 0; j < DEFAULT_ROWS_COLS; j++)
-                        sb.Append(Constants.NODE_EMPTY);
-                    scenarioData.Add(sb.ToString());
-                    sb.Clear();
-                }
-                
-                return new ScenarioWrapper()
-                {
-                    Title = "Untitled",
-                    Author = "No author",
-                    DateCreated = DateTime.Now,
-                    Data = scenarioData
-                };
-            }
-        }            
-
         private Toolbar toolbar;
         private Grid grid;
 
@@ -50,11 +25,10 @@ namespace PathfinderMG.Core.Source.States
             Texture2D buttonTexture = GameRoot.ContentMgr.Load<Texture2D>("Controls/Button");
             SpriteFont buttonFont = GameRoot.ContentMgr.Load<SpriteFont>("Fonts/DefaultFont");
 
-
             if (grid == null)
             {
                 Tuple<bool, Rectangle> previewData = Tuple.Create(false, Rectangle.Empty);
-                grid = new Grid(previewData, Constants.DEFAULT_NODE_SIZE, defaultScenario);
+                grid = new Grid(previewData, Constants.DEFAULT_NODE_SIZE, GetDefaultScenario());
             }
             else
                 this.grid = grid;
@@ -67,6 +41,37 @@ namespace PathfinderMG.Core.Source.States
         }
 
         #region Methods
+
+        private ScenarioWrapper GetDefaultScenario()
+        {
+            List<string> scenarioData = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            // Fill the list with empty spaces
+            for (int i = 0; i < DEFAULT_ROWS_COLS; i++)
+            {
+                for (int j = 0; j < DEFAULT_ROWS_COLS; j++)
+                {
+                    // Place start node and target node in the corners
+                    if (i == 0 && j == 0)
+                        sb.Append(Constants.NODE_START);
+                    else if (i == (DEFAULT_ROWS_COLS - 1) && j == (DEFAULT_ROWS_COLS - 1))
+                        sb.Append(Constants.NODE_TARGET);
+                    else
+                        sb.Append(Constants.NODE_EMPTY);
+                }
+                scenarioData.Add(sb.ToString());
+                sb.Clear();
+            }
+
+            return new ScenarioWrapper()
+            {
+                Title = "Untitled",
+                Author = "No author",
+                DateCreated = DateTime.Now,
+                Data = scenarioData
+            };
+        }
 
         #endregion
 
