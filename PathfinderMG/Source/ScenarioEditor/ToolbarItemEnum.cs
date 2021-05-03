9c5becdd7a878b8856bfc01a1fd7b68ecd355c14
Szymon Skórka <szymonskorka@outlook.com>
2021-05-03 15:39:57 +0200

    Add editor tool enum

---
+++ PathfinderMG/Source/ScenarioEditor/ToolbarItemEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfinderMG.Core.Source.ScenarioEditor
+{
+    public enum Tool
+    {
+        Wall = 1,
+        StartNode = 2,
+        TargetNode = 3,
+        Rubber = 4
+    }
+}
